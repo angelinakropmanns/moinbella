b888052b4c6ae22d7d0cf6d3ad577a59d6238233
angelinakropmanns <angelina.kropmanns@gmail.com>
2020-04-30 15:11:42 +0200

    adding basic styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components/macro';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Wrapper>
       <h1>Moin Bella!</h1>
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.section`
+  font: Malayalam Sangam MN;
+  font-size: 21;
+  color: #414756; 
+  background-color: #AED6DC;
+`
+
 
 export default App;
