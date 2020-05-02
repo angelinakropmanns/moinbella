d83cbc7793e041b158ffcfd45d93256d356fa970
angelinakropmanns <angelina.kropmanns@gmail.com>
2020-05-02 10:43:25 +0200

    basic styling change

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ProfileList from '../components/Profile/ProfileList'
 import Headline from '../components/Headline/Headline'
 
-export default function (onToggleProfile){
+export default function (){
     return (
         <>
         <Headline>Eure neuen Freunde</Headline>
