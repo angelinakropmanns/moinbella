e16a49a2670965a38bbe5e7d634af3c57f238fd8
angelinakropmanns <angelina.kropmanns@gmail.com>
2020-05-04 11:42:31 +0200

    Add Maps and change marker design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { Route, Switch } from 'react-router-dom'
 import Header from './components/Header/Header'
 import ProfilePage from './pages/ProfilePage'
+import MapsIcon from './components/MapsIcon/MapsIcon'
+import MapPage from './pages/MapPage'
+import ProfileIcon from './components/ProfileIcon/ProfileIcon'
 
 function App() {
   return (
@@ -11,6 +14,11 @@ function App() {
       <Switch>
         <Route exact path="/profiles">
           <ProfilePage />
+          <MapsIcon />
+        </Route>
+        <Route exact path="/maps">
+          <MapPage />
+          <ProfileIcon />
         </Route>
       </Switch>
       </Router>
---
+++ src/components/Maps.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Map, Marker, TileLayer } from 'react-leaflet'
+import { Icon } from 'leaflet'
+import mapsData from '../maps.json'
+import styled from 'styled-components/macro'
+
+const dog = new Icon ({
+    iconUrl: '/dog-icon.png',
+    iconSize: [25, 25]
+})
+
+export default function Maps(){ 
+    return (
+        <>
+        <MapStyled>
+            <Map center={[53.55, 9.99]} zoom={12}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+
+                {mapsData.map(place => (
+                    <Marker 
+                        key={place.id} 
+                        position={[
+                            place.coordinates[0], 
+                            place.coordinates[1]
+                        ]}
+                        icon={dog}
+                    />
+                ))}
+            </Map>
+            </MapStyled>
+        </>
+    )
+}
+
+const MapStyled = styled.section`
+    margin-top: 6px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 0;
+    .leaflet-container {
+        width: 360px;
+        height: 530px;
+    }
+`
