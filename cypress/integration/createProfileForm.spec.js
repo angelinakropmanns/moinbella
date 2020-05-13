3bc28f138481c550e7621fdd987a18a74001258e
Andreas Paech <me@andreas-paech.de>
2020-05-13 16:32:02 +0200

    remove exmaple tests and fix profile form test

--- cypress/integration/createProfileForm.spec.js
+++ cypress/integration/createProfileForm.spec.js
@@ -1,26 +1,27 @@
+const testPetName = "Hannah" + Math.floor(Math.random() * Math.floor(10));
+
 describe('after submit a new profile is created and form is cleared', () => {
-  it('the form is cleared after submit', () => {
+  it('should form is cleared after submit', () => {
     cy.visit('/create')
 
     cy.get('[data-cy=create_profile]')
-    cy.get('input[name="name"]').type('Hannah')
+    cy.get('input[name="name"]').type(testPetName)
     cy.get('input[name="mail"]').type('test@test.com')
     cy.get('input[name="plz"]').type('22222')
     cy.get('input[name="city"]').type('Hamburg')
-    cy.get('input[name="gender"]').type('Weiblich')
+    cy.get('select[name="gender"]').select('Weiblich')
     cy.get('input[name="breed"]').type('Golden Retriever')
     cy.get('[data-cy=create_profile]').submit()
     cy.get('input[name="name"]').should('have.value', '')
     cy.get('input[name="mail"]').should('have.value', '')
     cy.get('input[name="plz"]').should('have.value', '')
     cy.get('input[name="city"]').should('have.value', '')
-    cy.get('input[name="gender"]').should('have.value', '')
     cy.get('input[name="breed"]').should('have.value', '')
   })
 
   it('a new profile is added', () => {
     cy.visit('/profiles')
 
-    cy.contains('Hannah').should('exist')
+    cy.contains(testPetName).should('exist')
   })
 })
